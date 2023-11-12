850e8810b2832ba14fb26a81659dd7bcd2116965
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-11-12 13:03:15 -0700

    added an Apollo Provider to make requests work with the apollo server.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,46 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import "./App.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
 
-import Navbar from './components/Navbar';
+import { setContext } from "@apollo/client/link/context";
+
+import { Outlet } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import auth from "../../server/utils/auth";
+
+//Constructs our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+//request middleware that attaches the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
