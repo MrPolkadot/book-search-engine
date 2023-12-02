9df434c59f9c1cf91632433f8d6fc930e72ec27c
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-12-01 22:52:02 -0700

    edited connection variable again

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const { connect, connection } = require("mongoose");
 require("dotenv").config();
 
 
-const mongoURL = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/googlebooks';
+const mongoURL = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
 
 connect(mongoURL);
 
